228f676b3cbcf61d058cb4747a7e1ee577cbdc32
jackmassimiEdu <71198762+jackmassimiEdu@users.noreply.github.com>
2021-10-31 22:28:07 -0400

    Add files via upload

---
+++ CS1660FinalProject/CS1660.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs1660;
+
+/**
+ *
+ * @author JVMas
+ */
+public class CS1660 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Welcome to big data processing toolbox");
+        System.out.println("Make a selection");
+        System.out.println("1: Apache Hadoop");
+        System.out.println("2: Apache Spark");
+        System.out.println("3: Apache Notebook");
+        System.out.println("4: SonarQube and Sonar Scanner");
+        
+        //#TODO Make it actually do something
+        
+    }
+    
+}
